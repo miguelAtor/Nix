081c2eb59f556cf8ec8d4b8b60f5bfe1bf85e593
equiroz <equiroz@ciencias.unam.mx>
2020-04-21 19:37:29 -0500

    Actualizando 'Order' a 'Takeout'

--- nix/src/main/java/com/naat/nix/order/controller/TakeoutController.java
+++ nix/src/main/java/com/naat/nix/order/controller/TakeoutController.java
@@ -3,6 +3,6 @@ package com.naat.nix.order.controller;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class OrdersController {
+public class TakeoutController {
 
 }
--- nix/src/main/java/com/naat/nix/order/controller/TakeoutService.java
+++ nix/src/main/java/com/naat/nix/order/controller/TakeoutService.java
@@ -3,6 +3,6 @@ package com.naat.nix.order.controller;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrderService {
+public class TakeoutService {
 
 }
